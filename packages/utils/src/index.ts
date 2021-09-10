7c92c7b87b0e013aec18c861ff11eb2e7238cf44
Julien Capellari <julien@capellari.net>
2021-09-10 11:51:23 +0200

    add(utils): usePrevious

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,2 +1,3 @@
 export { useDeepMemo } from './useDeepMemo';
 export { useInterval } from './useInterval';
+export { usePrevious } from './usePrevious';
---
+++ packages/utils/tests/usePrevious.test.ts
@@ -0,0 +1,24 @@
+import { renderHook } from '@testing-library/react-hooks'
+
+import { usePrevious } from '../src'
+
+// Tests
+describe('usePrevious', () => {
+  it('should return previous value', () => {
+    // Render
+    const { result, rerender } = renderHook(({ value }) => usePrevious(value), {
+      initialProps: { value: 1 }
+    });
+
+    // Checks
+    expect(result.current).toBeNull();
+
+    // Update prop
+    rerender({ value: 2 });
+    expect(result.current).toBe(1);
+
+    // Update prop
+    rerender({ value: 5 });
+    expect(result.current).toBe(2);
+  });
+});
