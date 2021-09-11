a6a556b792a95549d485db95c4260ced03a0103d
Julien Capellari <julien@capellari.net>
2021-09-11 23:25:56 +0200

    update(api): test load option

--- packages/api/tests/useApi.test.ts
+++ packages/api/tests/useApi.test.ts
@@ -1,17 +1,17 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react-hooks';
 import axios from 'axios';
 
 import { useAPI } from '../src/useApi';
 
 // Setup
 beforeEach(() => {
-  jest.restoreAllMocks();
+  jest.resetAllMocks();
 });
 
 // Test suites
 describe('useApi.get', () => {
   // Tests
-  it('should print test', async () => {
+  it('should return api call result', async () => {
     // Mocks
     jest.spyOn(axios, 'get').mockResolvedValue({ data: 'test' });
 
@@ -27,6 +27,34 @@ describe('useApi.get', () => {
 
     expect(axios.get).toHaveBeenCalledTimes(1);
     expect(axios.get).toHaveBeenCalledWith('/api/test', {
+      cancelToken: expect.any(axios.CancelToken)
+    });
+
+    // After reload
+    act(() => {
+      result.current.reload();
+    });
+
+    expect(result.current).toEqual(expect.objectContaining({ data: 'test', loading: true }));
+
+    // After receive
+    await waitForNextUpdate();
+    expect(result.current).toEqual(expect.objectContaining({ data: 'test', loading: false }));
+
+    expect(axios.get).toHaveBeenCalledTimes(2);
+  });
+
+  it('should not load api call', () => {
+    // Mocks
+    jest.spyOn(axios, 'get').mockResolvedValue({ data: 'test' });
+
+    // Render
+    const { result } = renderHook(() => useAPI.get<string>('/api/test', {}, { load: false }));
+
+    // Checks
+    expect(result.current).toEqual(expect.objectContaining({ data: undefined, loading: true }));
+
+    expect(axios.get).not.toHaveBeenCalledWith('/api/test', {
       cancelToken: expect.any(axios.CancelToken)
     });
   });
