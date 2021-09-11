6f81648acd9bb5bef21dc8a89cfe7dc1647afcae
Julien Capellari <julien@capellari.net>
2021-09-11 23:40:16 +0200

    update(api): apply get test logic to head and options

--- packages/api/tests/useApi.test.ts
+++ packages/api/tests/useApi.test.ts
@@ -9,97 +9,101 @@ beforeEach(() => {
 });
 
 // Test suites
-describe('useApi.get', () => {
-  // Tests
-  it('should return api call result', async () => {
-    // Mocks
-    jest.spyOn(axios, 'get').mockResolvedValue({ data: 'test' });
+type GetMethods = 'get' | 'head' | 'options';
 
-    // Render
-    const { result, waitForNextUpdate } = renderHook(() => useAPI.get<string>('/api/test'));
+for (const method of ['get', 'head', 'options'] as (GetMethods)[]) {
+  describe(`useApi.${method}`, () => {
+    // Tests
+    it('should return api call result', async () => {
+      // Mocks
+      jest.spyOn(axios, method).mockResolvedValue({ data: 'test' });
 
-    // Checks
-    expect(result.current).toEqual(expect.objectContaining({ data: undefined, loading: true }));
+      // Render
+      const { result, waitForNextUpdate } = renderHook(() => useAPI[method]<string>('/api/test'));
 
-    // After receive
-    await waitForNextUpdate();
-    expect(result.current).toEqual(expect.objectContaining({ data: 'test', loading: false }));
+      // Checks
+      expect(result.current).toEqual(expect.objectContaining({ data: undefined, loading: true }));
 
-    expect(axios.get).toHaveBeenCalledTimes(1);
-    expect(axios.get).toHaveBeenCalledWith('/api/test', {
-      cancelToken: expect.any(axios.CancelToken)
+      // After receive
+      await waitForNextUpdate();
+      expect(result.current).toEqual(expect.objectContaining({ data: 'test', loading: false }));
+
+      expect(axios[method]).toHaveBeenCalledTimes(1);
+      expect(axios[method]).toHaveBeenCalledWith('/api/test', {
+        cancelToken: expect.any(axios.CancelToken)
+      });
+    });
+
+    it('should reload data', async () => {
+      // Mocks
+      jest.spyOn(axios, method).mockResolvedValue({ data: 'test' });
+
+      // Render
+      const { result, waitForNextUpdate } = renderHook(() => useAPI[method]<string>('/api/test'));
+
+      // Checks
+      expect(result.current).toEqual(expect.objectContaining({ data: undefined, loading: true }));
+
+      // After receive
+      await waitForNextUpdate();
+      expect(result.current).toEqual(expect.objectContaining({ data: 'test', loading: false }));
+
+      // After reload
+      act(() => {
+        result.current.reload();
+      });
+
+      expect(result.current).toEqual(expect.objectContaining({ data: 'test', loading: true }));
+
+      // After receive
+      await waitForNextUpdate();
+      expect(result.current).toEqual(expect.objectContaining({ data: 'test', loading: false }));
+
+      expect(axios[method]).toHaveBeenCalledTimes(2);
+    });
+
+    it('should not load api call', () => {
+      // Mocks
+      jest.spyOn(axios, method).mockResolvedValue({ data: 'test' });
+
+      // Render
+      const { result } = renderHook(() => useAPI[method]<string>('/api/test', {}, { load: false }));
+
+      // Checks
+      expect(result.current).toEqual(expect.objectContaining({ data: undefined, loading: true }));
+
+      expect(axios[method]).not.toHaveBeenCalledWith('/api/test', {
+        cancelToken: expect.any(axios.CancelToken)
+      });
+    });
+
+    it('should update cached value', async () => {
+      // Mocks
+      jest.spyOn(axios, method).mockResolvedValue({ data: 'test' });
+
+      // Render
+      const { result, waitForNextUpdate } = renderHook(() => useAPI[method]<string>('/api/test'));
+
+      // Checks
+      expect(result.current).toEqual(expect.objectContaining({ data: undefined, loading: true }));
+
+      // After receive
+      await waitForNextUpdate();
+      expect(result.current).toEqual(expect.objectContaining({ data: 'test', loading: false }));
+
+      // Update cached value
+      act(() => {
+        result.current.update('it\'s');
+      });
+
+      expect(result.current).toEqual(expect.objectContaining({ data: 'it\'s', loading: false }));
+
+      // Update cached value (with updator)
+      act(() => {
+        result.current.update((old) => `${old} working`);
+      });
+
+      expect(result.current).toEqual(expect.objectContaining({ data: 'it\'s working', loading: false }));
     });
   });
-
-  it('should reload data', async () => {
-    // Mocks
-    jest.spyOn(axios, 'get').mockResolvedValue({ data: 'test' });
-
-    // Render
-    const { result, waitForNextUpdate } = renderHook(() => useAPI.get<string>('/api/test'));
-
-    // Checks
-    expect(result.current).toEqual(expect.objectContaining({ data: undefined, loading: true }));
-
-    // After receive
-    await waitForNextUpdate();
-    expect(result.current).toEqual(expect.objectContaining({ data: 'test', loading: false }));
-
-    // After reload
-    act(() => {
-      result.current.reload();
-    });
-
-    expect(result.current).toEqual(expect.objectContaining({ data: 'test', loading: true }));
-
-    // After receive
-    await waitForNextUpdate();
-    expect(result.current).toEqual(expect.objectContaining({ data: 'test', loading: false }));
-
-    expect(axios.get).toHaveBeenCalledTimes(2);
-  });
-
-  it('should not load api call', () => {
-    // Mocks
-    jest.spyOn(axios, 'get').mockResolvedValue({ data: 'test' });
-
-    // Render
-    const { result } = renderHook(() => useAPI.get<string>('/api/test', {}, { load: false }));
-
-    // Checks
-    expect(result.current).toEqual(expect.objectContaining({ data: undefined, loading: true }));
-
-    expect(axios.get).not.toHaveBeenCalledWith('/api/test', {
-      cancelToken: expect.any(axios.CancelToken)
-    });
-  });
-
-  it('should update cached value', async () => {
-    // Mocks
-    jest.spyOn(axios, 'get').mockResolvedValue({ data: 'test' });
-
-    // Render
-    const { result, waitForNextUpdate } = renderHook(() => useAPI.get<string>('/api/test'));
-
-    // Checks
-    expect(result.current).toEqual(expect.objectContaining({ data: undefined, loading: true }));
-
-    // After receive
-    await waitForNextUpdate();
-    expect(result.current).toEqual(expect.objectContaining({ data: 'test', loading: false }));
-
-    // Update cached value
-    act(() => {
-      result.current.update('it\'s');
-    });
-
-    expect(result.current).toEqual(expect.objectContaining({ data: 'it\'s', loading: false }));
-
-    // Update cached value (with updator)
-    act(() => {
-      result.current.update((old) => `${old} working`);
-    });
-
-    expect(result.current).toEqual(expect.objectContaining({ data: 'it\'s working', loading: false }));
-  });
-});
+}
