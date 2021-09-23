0360473a9323b8797b1307184c76c938375cce77
Julien Capellari <julien@capellari.net>
2021-09-23 23:11:54 +0200

    update(api): handle post and patch methods

--- packages/api/src/api-resource.ts
+++ packages/api/src/api-resource.ts
@@ -18,8 +18,9 @@ export interface IApiResourceDeleteState<T> {
   remove: () => ApiPromise<T>;
 }
 
-export interface IApiResourcePutState<B, T> {
-  put: (body: B) => ApiPromise<T>;
+export type IApiPostMethod = 'patch' | 'post' | 'put';
+export type IApiResourcePostState<M extends IApiPostMethod, B, T> = {
+  [key in M]: (body: B) => ApiPromise<T>;
 }
 
 export type IApiResourceUrlBuilder<A> = A extends void ? string : (arg: A) => string;
@@ -27,7 +28,9 @@ export type IApiResourceUrlBuilder<A> = A extends void ? string : (arg: A) => st
 export type IApiResourceHookMethods<T, A, S extends IApiResourceGetState<T>> = {
   url: (arg: A) => string;
   delete: <NA = A>(url?: IApiResourceUrlBuilder<NA>) => IApiResourceHook<T, CombineArg<A, NA>, S & IApiResourceDeleteState<T>>;
-  put: <NB, NA = A>(url?: IApiResourceUrlBuilder<CombineArg<A, NA>>) => IApiResourceHook<T, CombineArg<A, NA>, S & IApiResourcePutState<NB, T>>;
+  patch: <NB, NA = A>(url?: IApiResourceUrlBuilder<CombineArg<A, NA>>) => IApiResourceHook<T, CombineArg<A, NA>, S & IApiResourcePostState<'patch', NB, T>>;
+  post: <NB, NA = A>(url?: IApiResourceUrlBuilder<CombineArg<A, NA>>) => IApiResourceHook<T, CombineArg<A, NA>, S & IApiResourcePostState<'post', NB, T>>;
+  put: <NB, NA = A>(url?: IApiResourceUrlBuilder<CombineArg<A, NA>>) => IApiResourceHook<T, CombineArg<A, NA>, S & IApiResourcePostState<'put', NB, T>>;
 }
 
 export type IApiResourceHook<T, A, S extends IApiResourceGetState<T>> = ((arg: A) => S) & IApiResourceHookMethods<T, A, S>;
@@ -39,8 +42,14 @@ function hookMethods<T, A, S extends IApiResourceGetState<T>>(url: (arg: A) => s
     delete<NA = A>(url?: IApiResourceUrlBuilder<NA>) {
       return addDeleteCall<T, CombineArg<A, NA>, S>(this, url as IApiResourceUrlBuilder<CombineArg<A, NA>>);
     },
+    patch<NB, NA = A>(url?: IApiResourceUrlBuilder<CombineArg<A, NA>>) {
+      return addPostCall<'patch', NB, T, CombineArg<A, NA>, S>('patch', this, url);
+    },
+    post<NB, NA = A>(url?: IApiResourceUrlBuilder<CombineArg<A, NA>>) {
+      return addPostCall<'post', NB, T, CombineArg<A, NA>, S>('post', this, url);
+    },
     put<NB, NA = A>(url?: IApiResourceUrlBuilder<CombineArg<A, NA>>) {
-      return addPutCall<NB, T, CombineArg<A, NA>, S>(this, url);
+      return addPostCall<'put', NB, T, CombineArg<A, NA>, S>('put', this, url);
     }
   };
 }
@@ -68,37 +77,32 @@ function addDeleteCall<T, A, S extends IApiResourceGetState<T>>(wrapped: IApiRes
   return Object.assign(useApiResource, hookMethods<T, A, S & IApiResourceDeleteState<T>>(wrapped.url));
 }
 
-function addPutCall<B, T, A, S extends IApiResourceGetState<T>>(wrapped: IApiResourceHook<T, A, S>, url?: IApiResourceUrlBuilder<A>): IApiResourceHook<T, A, S & IApiResourcePutState<B, T>> {
-  const putUrl: ((arg: A) => string) = url ? (typeof url === 'string' ? () => url : url) : wrapped.url;
+function addPostCall<M extends IApiPostMethod, B, T, A, S extends IApiResourceGetState<T>>(method: M, wrapped: IApiResourceHook<T, A, S>, url?: IApiResourceUrlBuilder<A>): IApiResourceHook<T, A, S & IApiResourcePostState<M, B, T>> {
+  const postUrl: ((arg: A) => string) = url ? (typeof url === 'string' ? () => url : url) : wrapped.url;
 
   // new hook
-  function useApiResource(arg: A): S & IApiResourcePutState<B, T> {
+  function useApiResource(arg: A): S & IApiResourcePostState<M, B, T> {
     // Memo
-    const url = useMemo(() => putUrl(arg), [useDeepMemo(arg)]); // eslint-disable-line react-hooks/exhaustive-deps
+    const url = useMemo(() => postUrl(arg), [useDeepMemo(arg)]); // eslint-disable-line react-hooks/exhaustive-deps
 
     // Api
     const all = wrapped(arg);
-    const { send } = useApi.put<B, T>(url);
+    const { send } = useApi[method]<B, T>(url);
 
     // Result
     return {
       ...all,
-      put: useCallback((data: B) => {
-        const prom = send(data);
-        const apip = prom.then((res) => {
+      [method]: useCallback((data: B) => {
+        return send(data).then((res) => {
           all.update(res);
 
           return res;
-        }) as ApiPromise<T>;
-
-        apip.cancel = prom.cancel;
-
-        return apip;
+        });
       }, [send, all.update]) // eslint-disable-line react-hooks/exhaustive-deps
-    };
+    } as S & IApiResourcePostState<M, B, T>;
   }
 
-  return Object.assign(useApiResource, hookMethods<T, A, S & IApiResourcePutState<B, T>>(wrapped.url));
+  return Object.assign(useApiResource, hookMethods<T, A, S & IApiResourcePostState<M, B, T>>(wrapped.url));
 }
 
 // Hook builder
