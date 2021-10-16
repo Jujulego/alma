538db7292c13b301209ef79297a12e06b8fcbc4c
Julien Capellari <julien@capellari.net>
2021-10-16 23:32:06 +0200

    update: rework api delete/post state

--- packages/api/src/api/request-api-promise.ts
+++ packages/api/src/api/request-api-promise.ts
@@ -1,26 +1,26 @@
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import { Dispatch, SetStateAction } from 'react';
-import { ApiState } from '../types';
+import { ApiResult, ApiState } from '../types';
 import { ApiPromise, makeApiPromise } from '../api-promise';
 
 // Utils
-export function makeRequestApiPromise<R, E>(promise: Promise<AxiosResponse<R>>, source: AbortController, setState: Dispatch<SetStateAction<ApiState<R, E>>>): ApiPromise<R> {
+export function makeRequestApiPromise<R, E = unknown>(promise: Promise<AxiosResponse<R>>, source: AbortController, setState: Dispatch<SetStateAction<ApiState>>): ApiPromise<ApiResult<R, E>> {
   return makeApiPromise(promise, () => source.abort())
-    .then((res): R => {
-      setState({ loading: false, status: res.status, data: res.data });
-      return res.data;
+    .then((res): ApiResult<R, E> => {
+      setState({ loading: false });
+      return { status: res.status, data: res.data };
     })
     .catch((error) => {
       if (axios.isAxiosError(error)) {
         const { response } = error as AxiosError<E>;
 
         if (response) {
-          setState({ loading: false, status: response.status, error: response.data });
-          throw error;
+          setState({ loading: false });
+          throw { status: response.status, error: response.data };
         }
       }
 
-      setState((old) => ({ ...old, loading: false }));
+      setState({ loading: false });
       throw error;
     });
 }
