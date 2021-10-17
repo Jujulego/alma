00e9ce2e1cf3fda4fa650493d03781bbb4292af8
Julien Capellari <julien@capellari.net>
2021-10-18 00:41:05 +0200

    feat(api): rework api resource hook methods

--- packages/api/src/api-resource.ts
+++ packages/api/src/api-resource.ts
@@ -1,129 +1,69 @@
 import { useDeepMemo } from '@jujulego/alma-utils';
 import { useCallback, useMemo } from 'react';
 
-import { ApiAutoLoadState, ApiLoadableHook, useApiAutoLoad, useApiGet } from './api';
+import { ApiAutoLoadState, ApiLoadableHook, useApiAutoLoad } from './api';
 import { ApiPromise } from './api-promise';
 import { ApiResponse } from './types';
 
-// Constants
-const API_GET_HOOKS = {
-  get: useApiGet,
-  // head: useApiHead,
-  // options: useApiOptions,
-  // delete: useApiDelete,
-};
-
-// const API_POST_HOOKS = {
-//   post: useApiPost,
-//   patch: useApiPatch,
-//   put: useApiPut,
-// };
-
 // Types
 export type ApiUrlBuilder<A> = (args: A) => string;
-export type ApiMerge<S, R> = (state: S | undefined, res: R | undefined) => S | undefined;
+export type ApiMerge<D, DM> = (state: D | undefined, res: DM) => D | undefined;
 
-export type ApiStateGetMethod<N extends string, RM> = {
-  [key in N]: () => ApiPromise<ApiResponse<RM>>
+export interface ApiQueryableHookState<D> {
+  loading: boolean;
+  send: (url?: string) => ApiPromise<ApiResponse<D>>;
 }
 
-// export type ApiStatePostMethod<N extends string, BM, RM, PM extends ApiParams> = {
-//   [key in N]: (body: BM, params?: PM) => ApiPromise<ApiResult<RM>>
-// }
+export type ApiQueryableHook<D> = (url: string) => ApiQueryableHookState<D>
 
-export type ApiHook<R, A, S extends ApiAutoLoadState<R>> = ((args: A) => S);// & ApiHookMethods<R, P, A, S>;
+export type ApiStateQueryMethod<N extends string, DM, AM> = {
+  [key in N]: (args: AM) => ApiPromise<ApiResponse<DM>>
+}
 
-export interface ApiHookMethods<R, A, S extends ApiAutoLoadState<R>> {
-  get<N extends string, RM = R>(name: N, merge: ApiMerge<R, RM>): ApiHook<R, A, S & ApiStateGetMethod<N, RM>>;
-  // head<N extends string, RM = R, PM extends ApiParams = P>(name: N, merge: ApiMerge<R, RM>): ApiHook<R, P, A, S & ApiStateGetMethod<N, RM, PM>>;
-  // options<N extends string, RM = R, PM extends ApiParams = P>(name: N, merge: ApiMerge<R, RM>): ApiHook<R, P, A, S & ApiStateGetMethod<N, RM, PM>>;
-  // delete<N extends string, RM = R, PM extends ApiParams = P>(name: N, merge: ApiMerge<R, RM>): ApiHook<R, P, A, S & ApiStateGetMethod<N, RM, PM>>;
-  // patch<N extends string, BM, RM = R, PM extends ApiParams = P>(name: N, merge: ApiMerge<R, RM>): ApiHook<R, P, A, S & ApiStatePostMethod<N, BM, RM, PM>>;
-  // post<N extends string, BM, RM = R, PM extends ApiParams = P>(name: N, merge: ApiMerge<R, RM>): ApiHook<R, P, A, S & ApiStatePostMethod<N, BM, RM, PM>>;
-  // put<N extends string, BM, RM = R, PM extends ApiParams = P>(name: N, merge: ApiMerge<R, RM>): ApiHook<R, P, A, S & ApiStatePostMethod<N, BM, RM, PM>>;
+export type ApiHook<D, A, S extends ApiAutoLoadState<D>> = ((args: A) => S) & ApiHookMethods<D, A, S>;
+
+export interface ApiHookMethods<D, A, S extends ApiAutoLoadState<D>> {
+  query<N extends string, DM = D, AM = void>(name: N, hook: ApiQueryableHook<DM>, url: null | string | ApiUrlBuilder<AM>, merge: ApiMerge<D, DM>): ApiHook<D, A, S & ApiStateQueryMethod<N, DM, AM>>;
 }
 
 // Utils
-function hookMethods<R, A, S extends ApiAutoLoadState<R>>(url: ApiUrlBuilder<A>): ApiHookMethods<R, A, S> {
+function hookMethods<D, A, S extends ApiAutoLoadState<D>>(url: ApiUrlBuilder<A>): ApiHookMethods<D, A, S> {
   return {
-    get<N extends string, RM>(name: N, merge: ApiMerge<R, RM>) {
-      return addGetCall<N, R, RM, A, S>(name, 'get', url, this, merge);
-    },
-//     head<N extends string, RM, PM extends ApiParams>(name: N, merge: ApiMerge<R, RM>) {
-//       return addGetCall<N, R, RM, P, PM, A, S>(name, 'head', url, this, merge);
-//     },
-//     options<N extends string, RM, PM extends ApiParams>(name: N, merge: ApiMerge<R, RM>) {
-//       return addGetCall<N, R, RM, P, PM, A, S>(name, 'options', url, this, merge);
-//     },
-//     delete<N extends string, RM, PM extends ApiParams>(name: N, merge: ApiMerge<R, RM>) {
-//       return addGetCall<N, R, RM, P, PM, A, S>(name, 'delete', url, this, merge);
-//     },
-//     patch<N extends string, BM, RM, PM extends ApiParams>(name: N, merge: ApiMerge<R, RM>) {
-//       return addPostCall<N, BM, R, RM, P, PM, A, S>(name, 'patch', url, this, merge);
-//     },
-//     post<N extends string, BM, RM, PM extends ApiParams>(name: N, merge: ApiMerge<R, RM>) {
-//       return addPostCall<N, BM, R, RM, P, PM, A, S>(name, 'post', url, this, merge);
-//     },
-//     put<N extends string, BM, RM, PM extends ApiParams>(name: N, merge: ApiMerge<R, RM>) {
-//       return addPostCall<N, BM, R, RM, P, PM, A, S>(name, 'put', url, this, merge);
-//     }
+    query<N extends string, DM, AM>(name: N, hook: ApiQueryableHook<DM>, builder: null | string | ApiUrlBuilder<AM>, merge: ApiMerge<D, DM>) {
+      return addQueryCall<N, D, DM, A, AM, S>(url, this, name, hook, builder, merge);
+    }
   };
 }
 
 // Hook modifier
-function addGetCall<N extends string, R, RM, A, S extends ApiAutoLoadState<R>>(name: N, method: keyof typeof API_GET_HOOKS, builder: ApiUrlBuilder<A>, wrapped: ApiHook<R, A, S>, merge: ApiMerge<R, RM>): ApiHook<R, A, S & ApiStateGetMethod<N, RM>> {
+function addQueryCall<N extends string, D, DM, A, AM, S extends ApiAutoLoadState<D>>(defaultBuilder: ApiUrlBuilder<A>, wrapped: ApiHook<D, A, S>, name: N, hook: ApiQueryableHook<DM>, builder: null | string | ApiUrlBuilder<AM>, merge: ApiMerge<D, DM>): ApiHook<D, A, S & ApiStateQueryMethod<N, DM, AM>> {
   // Hook
-  function useApiResource(args: A): S & ApiStateGetMethod<N, RM> {
+  function useApiResource(args: A): S & ApiStateQueryMethod<N, DM, AM> {
     // Url
     const sargs = useDeepMemo(args);
-    const url = useMemo(() => builder(sargs), [sargs]);
+    const defaultUrl = useMemo(() => defaultBuilder(sargs), [sargs]);
 
     // Api
-    const { send } = API_GET_HOOKS[method]<RM>(url);
+    const { send } = hook(defaultUrl);
     const all = wrapped(args);
 
     // Result
     const { update } = all;
 
     return Object.assign(all, {
-      [name]: useCallback(() => {
-        return send().then((res) => {
+      [name]: useCallback((args: AM) => {
+        const url = typeof builder === 'function' ? builder(args) : builder || undefined;
+
+        return send(url).then((res) => {
           update(old => merge(old, res.data));
           return res;
         });
       }, [send, update]),
-    } as ApiStateGetMethod<N, RM>);
+    } as ApiStateQueryMethod<N, DM, AM>);
   }
 
-  return Object.assign(useApiResource, hookMethods<R, A, S & ApiStateGetMethod<N, RM>>(builder));
+  return Object.assign(useApiResource, hookMethods<D, A, S & ApiStateQueryMethod<N, DM, AM>>(defaultBuilder));
 }
-
-// function addPostCall<N extends string, BM, R, RM, P extends ApiParams, PM extends ApiParams, A, S extends ApiGetState<R, P>>(name: N, method: keyof typeof API_POST_HOOKS, builder: ApiUrlBuilder<A>, wrapped: ApiHook<R, P, A, S>, merge: ApiMerge<R, RM>): ApiHook<R, P, A, S & ApiStatePostMethod<N, BM, RM, PM>> {
-//   // Hook
-//   function useApiResource(args: A, params?: P): S & ApiStatePostMethod<N, BM, RM, PM> {
-//     // Url
-//     const sargs = useDeepMemo(args);
-//     const url = useMemo(() => builder(sargs), [sargs]);
-//
-//     // Api
-//     const { send } = API_POST_HOOKS[method]<BM, RM, PM>(url);
-//     const all = wrapped(args, params);
-//
-//     // Result
-//     const { update } = all;
-//
-//     return Object.assign(all, {
-//       [name]: useCallback((body: BM, params?: PM) => {
-//         return send(body, params).then((res) => {
-//           update(old => merge(old, res.data));
-//           return res;
-//         });
-//       }, [send, update]),
-//     } as ApiStatePostMethod<N, BM, RM, PM>);
-//   }
-//
-//   return Object.assign(useApiResource, hookMethods<R, P, A, S & ApiStatePostMethod<N, BM, RM, PM>>(builder));
-// }
 
 // Hook builder
 export function apiResource<D, A = void>(hook: ApiLoadableHook<D>, url: string | ApiUrlBuilder<A>): ApiHook<D, A, ApiAutoLoadState<D>> {
