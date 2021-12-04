4af040739297b846703c2bc4cc70ac320b2a5605
Julien Capellari <julien.capellari@gmail.com>
2021-12-04 12:46:59 +0100

    chore(utils): bundle babelised code

--- packages/utils/gulpfile.ts
+++ packages/utils/gulpfile.ts
@@ -1,14 +1,15 @@
 import del from 'del';
 import gulp from 'gulp';
 import babel from 'gulp-babel';
-import rename from 'gulp-rename';
-import terser from 'gulp-terser';
 import typescript from 'gulp-typescript';
 import webpack from 'webpack-stream';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { WebpackPnpExternals } = require('webpack-pnp-externals');
+
 // Config
 const paths = {
-  entry: 'src/index.ts',
+  entry: 'dist/cjs/index.js',
   src: 'src/**/*.{ts,tsx}',
   deps: [
     '../../.pnp.*'
@@ -21,36 +22,44 @@ const dts = typescript.createProject('tsconfig.json', {
 });
 
 // Tasks
-gulp.task('clean', () => del(['lib', 'umd']));
+gulp.task('clean', () => del(['dist']));
 
 gulp.task('build:cjs', () => gulp.src(paths.src, { since: gulp.lastRun('build:cjs') })
   .pipe(babel({ envName: 'cjs' } as any))
-  .pipe(gulp.dest('lib'))
+  .pipe(gulp.dest('dist/cjs'))
 );
 
 gulp.task('build:esm', () => gulp.src(paths.src, { since: gulp.lastRun('build:esm') })
   .pipe(babel({ envName: 'esm' } as any))
-  .pipe(rename({ extname: '.mjs' }))
-  .pipe(gulp.dest('lib'))
-);
-
-gulp.task('build:umd', () => gulp.src(paths.entry, { since: gulp.lastRun('build:umd') })
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  .pipe(webpack(require('./webpack.config')))
-  .pipe(gulp.dest('umd'))
-  .pipe(terser({ output: { wrap_iife: true } }))
-  .pipe(rename({ suffix: '.min' }))
-  .pipe(gulp.dest('umd'))
+  .pipe(gulp.dest('dist/esm'))
 );
 
 gulp.task('build:types', () => gulp.src(paths.src, { since: gulp.lastRun('build:types') })
   .pipe(dts()).dts
-  .pipe(gulp.dest('lib'))
+  .pipe(gulp.dest('dist/types'))
+);
+
+gulp.task('bundle:umd', () => gulp.src(paths.entry, { since: gulp.lastRun('bundle:umd') })
+  .pipe(webpack({
+    mode: 'production',
+    output: {
+      filename: 'alma-utils.js',
+      library: {
+        name: '@jujulego/alma-utils',
+        type: 'umd'
+      }
+    },
+    externals: [
+      WebpackPnpExternals(),
+    ]
+  }))
+  .pipe(gulp.dest('dist/umd'))
 );
 
 gulp.task('build', gulp.series(
   'clean',
-  gulp.parallel('build:cjs', 'build:esm', 'build:types', 'build:umd'),
+  gulp.parallel('build:cjs', 'build:esm', 'build:types'),
+  'bundle:umd',
 ));
 
 gulp.task('watch', () => gulp.watch([paths.src, ...paths.deps], { ignoreInitial: false },
