957b60e161e63177df79d0e36f360de1f3cf9b25
Julien Capellari <julien.capellari@neoxia.com>
2022-01-06 17:07:59 +0100

    feature(utils): add interval callback

--- packages/utils/src/useInterval.ts
+++ packages/utils/src/useInterval.ts
@@ -4,20 +4,24 @@ import { useEffect, useState } from 'react';
  * Force the component to render regularly
  *
  * @param ms: milliseconds to wait before re-rendering the component
+ * @param cb: callback that will be called before triggering a new render
  * @returns: count of render since the component is mounted
  */
-export function useInterval(ms: number): number {
+export function useInterval(ms: number, cb?: () => void): number {
   // State
   const [count, setCount] = useState(0);
 
   // Effects
   useEffect(() => {
-    const interval = setInterval(() => setCount((old) => old + 1), ms);
+    const interval = setInterval(() => {
+      if (cb) cb();
+      setCount((old) => old + 1);
+    }, ms);
 
     return () => {
       clearInterval(interval);
     };
-  }, [ms]);
+  }, [cb, ms]);
 
   return count;
 }
