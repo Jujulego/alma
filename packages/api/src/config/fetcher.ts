34b19ccf738b46b56936830084a59add700c75b6
Julien Capellari <julien.capellari@neoxia.com>
2022-03-29 12:29:24 +0200

    feat(api): add Content-Type header when encoding data into json

--- packages/api/src/config/fetcher.ts
+++ packages/api/src/config/fetcher.ts
@@ -1,8 +1,9 @@
 import { ApiHeaders, ApiMethod, ApiRequest, ApiResponse } from '../types';
 
 // Utils
-function encodeBody(body?: unknown): BodyInit | undefined {
+function encodeBody(body: unknown, headers: Headers): BodyInit | undefined {
   if (body === undefined
+    || typeof body === 'string'
     || body instanceof ArrayBuffer
     || body instanceof Blob
     || body instanceof FormData
@@ -10,6 +11,9 @@ function encodeBody(body?: unknown): BodyInit | undefined {
   ) {
     return body;
   }
+
+  // Add content type header
+  headers.set('Content-Type', 'application/json; charset=utf-8');
 
   return JSON.stringify(body);
 }
@@ -25,16 +29,26 @@ function decodeHeaders(headers: Headers): ApiHeaders {
 }
 
 /**
- * Default fetcher uses fetch API to send requests
+ * Default fetcher uses fetch API to send requests.
+ * Will encode body in JSON, expect if it's one of the following types:
+ * - string
+ * - ArrayBuffer
+ * - Blob
+ * - FormData
+ * - URLSearchParams
+ *
+ * Those body types will be passed directly to fetch.
  *
  * @param req: request to send
  * @param signal: AbortSignal
  */
 export async function fetcher<M extends ApiMethod, B, D>(req: ApiRequest<M, B>, signal: AbortSignal): Promise<ApiResponse<D>> {
+  const headers = new Headers(req.headers);
+
   const res = await fetch(req.url, {
     method: req.method,
-    headers: new Headers(req.headers),
-    body: encodeBody(req.body),
+    headers,
+    body: encodeBody(req.body, headers),
     signal
   });
 
--- packages/api/tests/config/fetcher.test.ts
+++ packages/api/tests/config/fetcher.test.ts
@@ -5,52 +5,79 @@ import { fetcher } from '../../src';
 // Setup
 enableFetchMocks();
 
+let abort: AbortController;
+
 beforeEach(() => {
   global.fetch.resetMocks();
+
+  abort = new AbortController();
 });
 
 // Tests
 describe('ApiConfigContext defaults (with fetch)', () => {
-  it('should use fetch to send a get request', async () => {
-    global.fetch.mockResponse('"test"', { status: 200 });
+  describe('without body', () => {
+    it('should use fetch to send a get request (json response)', async () => {
+      global.fetch.mockResponse('"test"', { status: 200 });
 
-    // Mocks
-    const abort = new AbortController();
+      // Call fetcher
+      await expect(fetcher({ method: 'get', url: '/test', headers: {} }, abort.signal))
+        .resolves.toEqual({
+          status: 200,
+          headers: expect.anything(),
+          data: 'test'
+        });
 
-    // Call fetcher
-    await expect(fetcher({ method: 'get', url: '/test', headers: {} }, abort.signal))
-      .resolves.toEqual({
-        status: 200,
-        headers: expect.anything(),
-        data: 'test'
+      expect(global.fetch).toHaveBeenCalledWith('/test', {
+        method: 'get',
+        headers: expect.any(Headers),
+        signal: abort.signal
       });
-
-    expect(fetch).toHaveBeenCalledWith('/test', {
-      method: 'get',
-      headers: expect.any(Headers),
-      signal: abort.signal
     });
   });
 
-  it('should use fetch to send a post request', async () => {
-    global.fetch.mockResponse('"test"', { status: 200 });
+  describe('with body', () => {
+    beforeEach(() => {
+      global.fetch.mockResponse('"test"', { status: 200 });
+    });
 
-    // Mocks
-    const abort = new AbortController();
+    it('should use fetch to send a post request (text body)', async () => {
+      // Call fetcher
+      await expect(fetcher({ method: 'post', url: '/test', headers: {}, body: 'body' }, abort.signal))
+        .resolves.toEqual({
+          status: 200,
+          headers: expect.anything(),
+          data: 'test'
+        });
 
-    // Call fetcher
-    await expect(fetcher({ method: 'post', url: '/test', headers: {}, body: 'body' }, abort.signal))
-      .resolves.toEqual({
-        status: 200,
-        headers: expect.anything(),
-        data: 'test'
+      expect(global.fetch).toHaveBeenCalledWith('/test', {
+        method: 'post',
+        headers: expect.any(Headers),
+        body: 'body',
+        signal: abort.signal
+      });
+    });
+
+    it('should use fetch to send a post request (object body)', async () => {
+      const body = { test: true, id: 5 };
+
+      // Call fetcher
+      await expect(fetcher({ method: 'post', url: '/test', headers: {}, body }, abort.signal))
+        .resolves.toEqual({
+          status: 200,
+          headers: expect.anything(),
+          data: 'test'
+        });
+
+      expect(global.fetch).toHaveBeenCalledWith('/test', {
+        method: 'post',
+        headers: expect.any(Headers),
+        body: JSON.stringify(body),
+        signal: abort.signal
       });
 
-    expect(fetch).toHaveBeenCalledWith('/test', {
-      method: 'post',
-      headers: expect.any(Headers),
-      body: '"body"',
-      signal: abort.signal
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const { headers } = global.fetch.mock.calls[0][1]!;
+      expect((headers as Headers).get('Content-Type')).toBe('application/json; charset=utf-8');
     });
   });
 });
